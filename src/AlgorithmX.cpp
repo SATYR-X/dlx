84adc10dc454d513dc5482f740303fe9e0bd1032
LiuJianrui <ljr@LAPTOP-3HS3UO31>
2025-03-26 21:30:04 +0800

    ljr

--- src/AlgorithmX.cpp
+++ src/AlgorithmX.cpp
@@ -40,6 +40,10 @@ void extractCR( const std:: string& line, int &c, int& r){
     iss >> r;
 }
 
+bool startsWith(const std::string& str, char ch){
+    return !str.empty() && str[0] == ch;
+}
+
 int** processFileToMatrix(const std::string& filename,int& r,int& c){
     std::ifstring file(filename);
     if(!file.is_open()){
@@ -135,16 +139,45 @@ int **proFileToMat(const fs::path& filename, int& r, int& c){
     return matrix;
 }
 
+void freeMatrix(int** matrix, int rows){
+    for(int i = 0;i < rows; i++){
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+}
+
 int main(){
     try{
         const std::string folderPath = "../set_partitioning_benchmarks";
-         for(const auto& entry : fs::directory_iterator(folderPath)){
+        for(const auto& entry : fs::directory_iterator(folderPath)){
             if(entry.is_regular_file() && enrty.path().extension() == ".txt"){
                 fs::path filepath = entry.path();
                 int rows,cols;
                 int **matrix = proFileToMat(filepath, rows, cols);
                 std::cout<< rows << " " << cols << std::endl;
+
+                ExactCoverMatrix ecm(rows, cols, matrix);
+
+                std::vector<int> solution;
+                int count = 0;
+                auto start =  std::chrono::high_resolution_clock::now();
+                ecm.search(solution, count);
+                
+                auto end = std::chrono::high_resolution_clock::now();
+                auto duration = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+
+                std::cout<< "File name: " << entry.path().filename().string() << std::endl;
+                std::cout<< "Search Compulation Time: " << duration.count() << " seconds.";
+                std::cout<< "Solution Numbers: " << count << std::endl;
+                std::cout<< std::endl;
+
+                freeMatrix(matrix, rows);
+                std::cout<<std::endl;
             }
          }
+    } catch(const std::exception& e){
+        std::cerr << "错误：" << e.what() << '\n';
     }
+
+    return 0;
 }
